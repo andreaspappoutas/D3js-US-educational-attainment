f45f7f9780df04f3ca47f614981ffd87b090d171
andreaspappoutas <44147982+andreaspappoutas@users.noreply.github.com>
2018-10-30 02:20:46 +0200

    Update script_2.js

--- assets/scripts/script_2.js
+++ assets/scripts/script_2.js
@@ -11,7 +11,7 @@ var margin = {top: 30, right: 0, bottom: 20, left: 0},
     formatNumber = d3.format(","),
     transitioning;
 
-var color = d3.scaleLinear().domain([0, 1/4*5000000, 2/4*5000000, 3/4*5000000, 5000000]).range(["#93f791", "#4dc19b", "#b21aa5", "#004409"]);
+var color = d3.scaleLinear().domain([0, 1/4*5000000, 2/4*5000000, 3/4*5000000, 5000000]).range(["#ff00ae", "#b8c100", "#d896e8", "#5fb0d8"]);
 
 // sets x and y scale to determine size of visible boxes
 var x = d3.scaleLinear()
@@ -176,7 +176,7 @@ d3.json("assets/data/us.json", function(data) {
             treemap(root
             .sum(function (d) {
                 if (treeSumSortType == "number") {
-                    color = d3.scaleLinear().domain([0, 1/4*5000000, 2/4*5000000, 3/4*5000000, 5000000]).range(["#93f791", "#4dc19b", "#b21aa5", "#004409"]);
+                    color = d3.scaleLinear().domain([0, 1/4*5000000, 2/4*5000000, 3/4*5000000, 5000000]).range(["#ff00ae", "#b8c100", "#d896e8", "#5fb0d8"]);
                     return d["Total College"];
                 } else if (treeSumSortType == "percent") {
                     color = d3.scaleLinear().domain([0, 1/4*50, 2/4*50, 3/4*50, 50]).range(["#275b68", "#961678", "#c4054b", "#623a72"]);
