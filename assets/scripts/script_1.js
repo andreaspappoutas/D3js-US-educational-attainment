e42dadcf2a84d7e034165cf951c5b0af9446947e
andreaspappoutas <44147982+andreaspappoutas@users.noreply.github.com>
2018-11-06 01:18:42 +0200

    Update script_1.js

--- assets/scripts/script_1.js
+++ assets/scripts/script_1.js
@@ -2,8 +2,8 @@
 // Data Visualization III - Pie Chart
 
 var donut = donutChart()
-        .width(960)
-        .height(450)
+        .width(100)
+        .height(100)
         .cornerRadius(0) // sets how rounded the corners are on each slice
         .padAngle(0.005) // effectively dictates the gap between slices
         .variable('Percent')
